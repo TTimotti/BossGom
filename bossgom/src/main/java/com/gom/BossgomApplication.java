b709f31d90d5e952b6875555c237fd13fc69a0cc
itwill <itwill@DESKTOP-68VI7GK>
2023-02-06 18:21:31 +0900

    2023-02-06 Boss Gom Website - 김지훈

---
+++ bossgom/src/main/java/com/gom/BossgomApplication.java
@@ -0,0 +1,13 @@
+package com.gom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BossgomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BossgomApplication.class, args);
+	}
+
+}
---
+++ bossgom/src/test/java/com/gom/BossgomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BossgomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
