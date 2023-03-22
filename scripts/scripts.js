f45ded50f8aa4d64e2957b1f3df2010adf6ff701
bestt <bestt@timkim>
2023-03-22 16:07:08 +0900

    update MapApi

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,13 @@
-/*!
-* Start Bootstrap - One Page Wonder v6.0.5 (https://startbootstrap.com/theme/one-page-wonder)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
+/**
+ *  createdDate : 2023-02-13
+ *  modifiedDate : 2023-03-22
+ *  author : TT
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+// 카카오 지도
+new daum.roughmap.Lander({
+							        "timestamp" : "1679467482006",
+							        "key" : "2e6j9",
+							        "mapWidth" : "640",
+							        "mapHeight" : "360"
+							    }).render();
