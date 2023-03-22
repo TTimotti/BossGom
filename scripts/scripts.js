c9405e8765f600064c82b79e48a6a54a8d2da983
bestt <bestt@timkim>
2023-03-22 16:09:23 +0900

    update js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,11 +3,3 @@
  *  modifiedDate : 2023-03-22
  *  author : TT
 */
-
-// 카카오 지도
-new daum.roughmap.Lander({
-							        "timestamp" : "1679467482006",
-							        "key" : "2e6j9",
-							        "mapWidth" : "640",
-							        "mapHeight" : "360"
-							    }).render();
